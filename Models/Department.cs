66ace7243f460b02cdeafb573a2ece1b2e4ddc3c
Felipe Pereira <felipepereiracg@gmail.com>
2019-07-04 21:44:15 -0300

    Adicionando outras Entidades

--- Models/Department.cs
+++ Models/Department.cs
@@ -9,5 +9,28 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sallers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSaller(Seller sr)
+        {
+            Sallers.Add(sr);
+        }
+
+        public double TotalSales(DateTime inital,DateTime final)
+        {
+            return Sallers.Sum(seller => seller.TotalSales(inital,final));
+        }
     }
 }
